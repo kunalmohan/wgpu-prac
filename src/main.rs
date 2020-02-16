1f2421e8aad242be93e0335563e6016f126f3e79
Kunal Mohan <kunalmohan99@gmail.com>
2020-02-16 15:23:03 +0530

    render simple cube

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,28 @@ use winit::{
     dpi::PhysicalSize,
 };
 
+#[cfg_attr(rustfmt, rustfmt_skip)]
+pub const OPENGL_TO_WGPU_MATRIX: cgmath::Matrix4<f32> = cgmath::Matrix4::new(
+    1.0, 0.0, 0.0, 0.0,
+    0.0, -1.0, 0.0, 0.0,
+    0.0, 0.0, 0.5, 0.0,
+    0.0, 0.0, 0.5, 1.0,
+);
+
 fn main() {
     let event_loop = EventLoop::new();
     let window = Window::new(&event_loop).unwrap();
 
-    let mut cube = Cube::new(window);
+    let id = window.id();
+
+    let mut cube = Cube::new(&window);
 
     event_loop.run(move |event, _, control_flow| {
         match event {
             Event::WindowEvent {
                 ref event,
                 window_id,
-            } if window_id == window.id() => if cube.input(event) {
+            } if window_id == id => if cube.input(event) {
                 *control_flow = ControlFlow::Wait;
             } else {
                 match event {
@@ -44,14 +54,14 @@ fn main() {
                         *control_flow = ControlFlow::Wait;
                     },
                     _ => *control_flow = ControlFlow::Wait,
-                },
+                }
             }
             Event::MainEventsCleared => {
                 cube.update();
                 cube.render();
                 *control_flow = ControlFlow::Wait;
-            }
-            - => *control_flow = ControlFlow::Wait;
+            },
+            _ => *control_flow = ControlFlow::Wait,
         }
     });
 }
@@ -63,16 +73,20 @@ struct Cube {
     queue: wgpu::Queue,
     swap_chain: wgpu::SwapChain,
     sc_desc: wgpu::SwapChainDescriptor,
-  //  buffer: wgpu::Buffer,
+    vertex_buffer: wgpu::Buffer,
+    index_buffer: wgpu::Buffer,
+    num_indices: u32,
+    render_pipeline: wgpu::RenderPipeline,
+    camera: Camera,
 }
 
 impl Cube {
-    fn new(window: Window) -> Self {
+    fn new(window: &Window) -> Self {
         let inner_size = window.inner_size();
         let adapter = wgpu::Adapter::request(&wgpu::RequestAdapterOptions {
             ..Default::default()
         }).unwrap();
-        let surface = wgpu::Surface::create(&window);
+        let surface = wgpu::Surface::create(window);
 
         let (device, queue) = adapter.request_device(&wgpu::DeviceDescriptor {
             extensions: wgpu::Extensions {
@@ -83,17 +97,80 @@ impl Cube {
 
         let sc_desc = wgpu::SwapChainDescriptor {
             usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT,
-            format: wgpu::TextureFormat::Rgba8Snorm,
+            format: wgpu::TextureFormat::Bgra8UnormSrgb,
             width: inner_size.width,
             height: inner_size.height,
             present_mode: wgpu::PresentMode::Vsync,
         };
         let swap_chain = device.create_swap_chain(&surface, &sc_desc);
 
-        /*let buffer = device.create_buffer(&wgpu::BufferDescriptor {
-            size: ,
-            usage: wgpu::BufferUsage::
-        })*/
+        let vertex_buffer = device.create_buffer_mapped(VERTICES.len(), wgpu::BufferUsage::VERTEX).fill_from_slice(VERTICES);
+
+        let index_buffer = device.create_buffer_mapped(INDICES.len(), wgpu::BufferUsage::INDEX).fill_from_slice(INDICES);
+
+        let num_indices = INDICES.len() as u32;
+
+        let vs_src = include_str!("shader.vert");
+        let fs_src = include_str!("shader.frag");
+
+        let vs_spriv = glsl_to_spirv::compile(vs_src, glsl_to_spirv::ShaderType::Vertex).unwrap();
+        let fs_spirv = glsl_to_spirv::compile(fs_src, glsl_to_spirv::ShaderType::Fragment).unwrap();
+
+        let vs_data = wgpu::read_spirv(vs_spriv).unwrap();
+        let fs_data = wgpu::read_spirv(fs_spirv).unwrap();
+
+        let vs_module = device.create_shader_module(&vs_data);
+        let fs_module = device.create_shader_module(&fs_data);
+
+        let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor{
+            bind_group_layouts: &[],
+        });
+
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor{
+            layout: &render_pipeline_layout,
+            vertex_stage: wgpu::ProgrammableStageDescriptor {
+                module: &vs_module,
+                entry_point: "main",
+            },
+            fragment_stage: Some(wgpu::ProgrammableStageDescriptor {
+                module: &fs_module,
+                entry_point: "main",
+            }),
+            rasterization_state: Some(wgpu::RasterizationStateDescriptor{
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: wgpu::CullMode::None,
+                depth_bias: 0,
+                depth_bias_slope_scale: 0.0,
+                depth_bias_clamp: 0.0,
+            }),
+            primitive_topology: wgpu::PrimitiveTopology::TriangleList,
+            color_states: &[
+                wgpu::ColorStateDescriptor{
+                    format: wgpu::TextureFormat::Bgra8UnormSrgb,
+                    alpha_blend: wgpu::BlendDescriptor::REPLACE,
+                    color_blend: wgpu::BlendDescriptor::REPLACE,
+                    write_mask: wgpu::ColorWrite::ALL,
+                },
+            ],
+            depth_stencil_state: None,
+            index_format: wgpu::IndexFormat::Uint16,
+            vertex_buffers: &[
+                Vertex::desc(),
+            ],
+            sample_count: 1,
+            sample_mask: !0,
+            alpha_to_coverage_enabled: false,
+        });
+
+        let camera = Camera {
+            eye: (0.0, 1.0, -2.0).into(),
+            target: (0.0, 0.0, 0.0).into(),
+            up: cgmath::Vector3::unit_y(),
+            aspect: sc_desc.width as f32 / sc_desc.height as f32,
+            fovy: 45.0,
+            znear: 0.1,
+            zfar: 100.0,
+        };
 
         Self {
             adapter,
@@ -102,6 +179,11 @@ impl Cube {
             sc_desc,
             swap_chain,
             surface,
+            vertex_buffer,
+            index_buffer,
+            num_indices,
+            render_pipeline,
+            camera,
         }
     }
 
@@ -111,15 +193,119 @@ impl Cube {
         self.swap_chain = self.device.create_swap_chain(&self.surface, &self.sc_desc);
     }
 
-    fn render(&self) {
+    fn render(&mut self) {
+        let frame = self.swap_chain.get_next_texture();
+        let mut encoder = self.device.create_command_encoder(&wgpu::CommandEncoderDescriptor{
+            todo: 0,
+        });
+
+        {    
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor{
+                color_attachments: &[
+                    wgpu::RenderPassColorAttachmentDescriptor {
+                        attachment: &frame.view,
+                        resolve_target: None,
+                        load_op: wgpu::LoadOp::Clear,
+                        store_op: wgpu::StoreOp::Store,
+                        clear_color: wgpu::Color {
+                            r: 0.1,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0,
+                        },
+                    }
+                ],
+                depth_stencil_attachment: None,
+            });
+
+            render_pass.set_pipeline(&self.render_pipeline);
+            render_pass.set_vertex_buffers(0, &[(&self.vertex_buffer, 0)]);
+            render_pass.set_index_buffer(&self.index_buffer, 0);
+            render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
+        }
+
+        self.queue.submit(&[
+            encoder.finish()
+        ]);
+    }
+
+    fn update(&mut self) {
 
     }
 
-    fn update(&self) {
-
-    }
-
-    fn input(&self) {
+    fn input(&mut self, event: &WindowEvent) -> bool {
         false
+    }
+}
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+struct Vertex {
+    position: [f32; 3],
+    color: [f32; 3],
+}
+
+const VERTICES: &[Vertex] = &[
+    Vertex { position: [0.5, -0.5, 0.0], color: [0.2, 0.4, 0.7] },
+    Vertex { position: [0.5, 0.5, 0.0], color: [0.2, 0.4, 0.1] },
+    Vertex { position: [-0.5, 0.5, 0.0], color: [0.2, 0.9, 0.7] },
+    Vertex { position: [-0.5, -0.5, 0.0], color: [0.9, 0.4, 0.7] },
+    Vertex { position: [0.5, -0.5, 0.5], color: [0.2, 0.8, 0.2] },
+    Vertex { position: [0.5, 0.5, 0.5], color: [0.8, 0.1, 0.7] },
+    Vertex { position: [-0.5, 0.5, 0.5], color: [0.1, 0.6, 0.2] },
+    Vertex { position: [-0.5, -0.5, 0.5], color: [0.9, 0.3, 1.0] },
+];
+
+const INDICES: &[u16] = &[
+    0, 1, 2,
+    0, 2, 3,
+    0, 6, 4,
+    0, 6, 2,
+    4, 6, 5,
+    4, 6, 7,
+    2, 5, 1,
+    2, 5, 6,
+    0, 5, 1,
+    0, 5, 4,
+    2, 7, 3,
+    2, 7, 6,
+];
+
+impl Vertex {
+    fn desc<'a>() -> wgpu::VertexBufferDescriptor<'a> {
+        wgpu::VertexBufferDescriptor {
+            stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
+            step_mode: wgpu::InputStepMode::Vertex,
+            attributes: &[
+                wgpu::VertexAttributeDescriptor {
+                    offset: 0u64,
+                    format: wgpu::VertexFormat::Float3,
+                    shader_location: 0u32,
+                },
+                wgpu::VertexAttributeDescriptor {
+                    offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
+                    format: wgpu::VertexFormat::Float3,
+                    shader_location: 1u32, 
+                },
+            ],
+        }
+    }
+}
+
+struct Camera {
+    eye: cgmath::Point3<f32>,
+    target: cgmath::Point3<f32>,
+    up: cgmath::Vector3<f32>,
+    aspect: f32,
+    fovy: f32,
+    znear: f32,
+    zfar: f32,
+}
+
+impl Camera {
+    fn build_view_projection_matrix(&self) -> cgmath::Matrix4<f32> {
+        let view = cgmath::Matrix4::look_at(self.eye, self.target, self.up);
+        let proj = cgmath::perspective(cgmath::Deg(self.fovy), self.aspect, self.znear, self.zfar);
+        return OPENGL_TO_WGPU_MATRIX * proj * view;
     }
 }
